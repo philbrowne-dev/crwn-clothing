4d07f55143cdade5188d987218b977137d5f4073
Phil Browne <phil.browne@millbrookhealthcare.co.uk>
2020-11-02 12:45:33 +0000

    Updating app with shop page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { BrowserRouter,Switch , Route } from 'react-router-dom';
+import { Switch , Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
-const HatsPage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
    <div>
      <Switch>
        <Route exact path='/' component={HomePage}></Route>
-       <Route path='/hats' component={HatsPage}></Route>
+       <Route path='/shop' component={ShopPage}></Route>
+
      </Switch>
    </div>
   );
